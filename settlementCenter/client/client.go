92250e05b672bfe5819dd7b1e196cf27b7e20794
Nicke-lucky <13965882371@163.com>
2020-05-07 20:29:35 +0800

    xml压缩

--- settlementCenter/client/client.go
+++ settlementCenter/client/client.go
@@ -1,22 +1,35 @@
 package client
 
 import (
+	"bytes"
+	"compress/flate"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"net"
+	"os"
 )
 
-func Client() {
+func NewClient() net.Conn {
 	//1、客户端主动连接服务器 http://127.0.0.1:8808/ysjyxx0000011.xml
 	conn, err := net.Dial("tcp", "127.0.0.1:8808")
 	if err != nil {
-		fmt.Println("Dial err:", err)
-		return
+		log.Fatal("Dial 联网中心 err:", err)
+		return nil
 	}
-	fmt.Println("tcp连接成功")
+	log.Fatal("tcp连接成功")
 	defer conn.Close() //条件反射出来 延迟关闭
+	return conn
+}
+
+func Client() {
+	conn := NewClient()
 
 	//2、模拟浏览器，组织一个最简单的请求报文。包含请求行，请求头，空行即可。
+	//requestHttpHeader := "GET /ysjyxx0000011.xml HTTP/1.1\r\nHost:127.0.0.1:8808\r\n\r\n"
 	requestHttpHeader := "GET /ysjyxx0000011.xml HTTP/1.1\r\nHost:127.0.0.1:8808\r\n\r\n"
+	// 准备原始记录的xml数据包的请求包
 	//3、给服务器发送请求报文
 	conn.Write([]byte(requestHttpHeader))
 
@@ -30,4 +43,62 @@ func Client() {
 	}
 	//5、打印观察
 	fmt.Printf("#\n%s#", string(buf[:n]))
+}
+
+func ReadAndCompress(filename string) {
+	filepath := "../sendxml/" + filename
+
+	fbuf := make([]byte, 1024*4)
+
+	file, fErr := os.OpenFile(filepath, os.O_RDWR, 0777)
+	if fErr != nil {
+		log.Fatal("Open sendxml err:", fErr)
+		return
+	}
+	defer file.Close()
+	// 向缓存区写入数据
+	Size, readErr := file.Read(fbuf)
+	if readErr != nil {
+		if readErr == io.EOF {
+			log.Fatal("EOF", readErr)
+		} else {
+			log.Fatal("Read:", readErr)
+		}
+		return
+	}
+	// 一个缓冲区压缩的内容
+	buf := bytes.NewBuffer(nil)
+
+	//创建文件
+	fw, f_werr := os.Create(filename)
+	if f_werr != nil {
+		log.Fatal("Read:", f_werr)
+	}
+	defer fw.Close()
+	fw.Write()
+	// 创建一个flate.Writer，压缩级别最好
+	flateWrite, err := flate.NewWriter(buf, flate.BestCompression)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer flateWrite.Close()
+	// 写入待压缩内容
+	flateWrite.Write(fbuf[:Size])
+	flateWrite.Flush()
+	//flateWrite.
+	fmt.Println(fbuf)
+}
+
+//XML文件 按照 LZ77 方式打包
+func Compress() {
+	myfolder := `../sendxml`
+	files, _ := ioutil.ReadDir(myfolder)
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		} else {
+			fmt.Println(file.Name())
+			ReadAndCompress(file.Name())
+		}
+	}
 }
--- settlementCenter/client/client_test.go
+++ settlementCenter/client/client_test.go
@@ -5,3 +5,7 @@ import "testing"
 func TestClient(t *testing.T) {
 	Client()
 }
+
+func TestCompress(t *testing.T) {
+	Compress()
+}
