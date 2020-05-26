e553e4fac9d9f0afd54fc93eaed49df4a6eba508
Nicke-lucky <13965882371@163.com>
2020-05-26 21:08:36 +0800

    fix 时间转换

--- src/CenterSettlement-go/common/timeDiff_test.go
+++ src/CenterSettlement-go/common/timeDiff_test.go
@@ -1,13 +1,16 @@
 package common
 
 import (
+	log "github.com/sirupsen/logrus"
 	"testing"
-	"time"
 )
 
 func TestTimeDifference(t *testing.T) {
-	t1 := time.Now().Format("2006-01-02 15:04:05")
-	time.Sleep(time.Second * 10)
-	t2 := time.Now().Format("2006-01-02 15:04:05")
-	TimeDifference(t2, t1)
+	//进入时间   出去时间
+	TimeDifference("2020-03-14 13:13:12", "2020-05-20 15:10:05")
+	h := getHourDiffer("2020-03-14 13:13:12", "2020-05-20 15:10:05")
+	m := getMDiffer("2020-03-14 13:13:12", "2020-05-20 15:10:05")
+	s := getSDiffer("2020-03-14 13:13:12", "2020-05-20 15:10:05")
+	log.Println(h/24, m/24/60, s/24/60/60)
+	log.Println(h, m, s)
 }
--- src/CenterSettlement-go/common/timeDifference.go
+++ src/CenterSettlement-go/common/timeDifference.go
@@ -1,12 +1,64 @@
 package common
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"time"
 )
 
-func TimeDifference(intime time.Time, outTime time.Time) {
-	t := outTime.Sub(intime)
-	log.Println("时间差", t)
+func TimeDifference(intime string, outtime string) {
+	inTime, _ := time.Parse("2006-01-02 15:04:05", intime)
+	outTime, _ := time.Parse("2006-01-02 15:04:05", outtime)
+	t := outTime.Sub(inTime)
 
+	log.Println("时间差", t)
+	log.Println("时间差 小时", t.Hours(), "=========", t.Hours())
+	log.Println("时间差  分", t.Minutes())
+	log.Println("时间差  秒", t.Seconds())
+}
+
+func FDate() {
+	a, _ := time.Parse("2006-01-02", "2018-09-01")
+	b, _ := time.Parse("2006-01-02", "2018-09-02")
+	d := a.Sub(b)
+	fmt.Println(d.Hours() / 24)
+}
+
+func getHourDiffer(start_time, end_time string) int64 {
+	var hour int64
+	t1, err := time.ParseInLocation("2006-01-02 15:04:05", start_time, time.Local)
+	t2, err := time.ParseInLocation("2006-01-02 15:04:05", end_time, time.Local)
+	if err == nil && t1.Before(t2) {
+		diff := t2.Unix() - t1.Unix() //
+		hour = diff / 3600
+		return hour
+	} else {
+		return hour
+	}
+}
+
+func getMDiffer(start_time, end_time string) int64 {
+	var hour int64
+	t1, err := time.ParseInLocation("2006-01-02 15:04:05", start_time, time.Local)
+	t2, err := time.ParseInLocation("2006-01-02 15:04:05", end_time, time.Local)
+	if err == nil && t1.Before(t2) {
+		diff := t2.Unix() - t1.Unix() //
+		hour = diff / 60
+		return hour
+	} else {
+		return hour
+	}
+}
+
+func getSDiffer(start_time, end_time string) int64 {
+	var hour int64
+	t1, err := time.ParseInLocation("2006-01-02 15:04:05", start_time, time.Local)
+	t2, err := time.ParseInLocation("2006-01-02 15:04:05", end_time, time.Local)
+	if err == nil && t1.Before(t2) {
+		diff := t2.Unix() - t1.Unix() //
+		hour = diff
+		return hour
+	} else {
+		return hour
+	}
 }
